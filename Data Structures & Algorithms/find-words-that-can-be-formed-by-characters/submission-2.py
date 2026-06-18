d441f68437a05ab5841dda59b2ba6564d56d7488
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2026-06-18 15:50:16 +0700

    Add: find-words-that-can-be-formed-by-characters - submission-2

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+
+        freqChars = {}
+
+        res = 0
+
+        for char in chars:
+            freqChars[char] = freqChars.get(char, 0) + 1
+        
+        for word in words:
+            flag = 1
+
+            freqWord = {}
+
+            temp = freqChars.copy()
+
+            for w in word:
+                freqWord[w] = freqWord.get(w, 0) + 1
+            
+            for w in word:
+                if w not in temp:
+                    flag = 0
+                    break
+                else:
+                    temp[w] -= 1
+                    if temp[w] < 0:
+                        flag = 0
+                        break
+            
+            if flag: 
+                res += len(word)
+
+        return res
+
+            
+
+            
+
+        
