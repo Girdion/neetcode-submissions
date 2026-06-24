3e82fcdb6fc02f3d32f6ff36407ba4633217fef3
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2026-06-24 22:52:58 +0700

    Add: crawler-log-folder - submission-2

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        
+        cnt = 0
+        for log in logs:
+            if log == '../':
+                cnt -= 1
+            elif log == './':
+                continue
+            else:
+                cnt += 1
+            if cnt < 0 : cnt = 0
+
+        if cnt < 0: return 0
+        return cnt
+        
