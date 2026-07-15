86be1885073895e417d921c94e60bf5dadcd77a3
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2026-07-15 09:09:23 +0700

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+        minDiff = max(arr)
+
+        res = []
+
+        for i in range(k):
+            diff = abs(arr[i] - x)
+            
+            minDiff = min(minDiff, diff)
+
+            res.append(arr[i])
+        
+        for right in range(k, len(arr)):
+
+            diff = abs(arr[right] - x)
+
+            if diff < abs(res[0] - x):
+                res.pop(0)
+                res.append(arr[right])
+            
+            minDiff = min(minDiff, diff)
+        
+        return res
+
