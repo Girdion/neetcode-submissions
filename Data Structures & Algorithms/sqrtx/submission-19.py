8e8f30e652e28d11c2ccd157a43f674ed37521fa
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2026-07-17 08:45:43 +0700

    Add: sqrtx - submission-19

---
+++ Data Structures & Algorithms/sqrtx/submission-19.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x+1
+
+        while l < r:
+
+            m = (l + r) // 2
+
+            if m * m > x:
+                r = m
+            else:
+                l = m + 1
+        
+        return l-1
+        
