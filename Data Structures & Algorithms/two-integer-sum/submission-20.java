1e3579063609b7dc2528810c08b8701379fd7841
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2026-08-28 17:17:47 +0700

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.java
@@ -0,0 +1,24 @@
+
+// HashMap
+// Integer -> Integer
+// Key: Number, Value: Index
+// Complement => Target - Num is in HashMap, kita return current index
+// sama return HashMap[complement]
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if(hashMap.containsKey(complement)){
+                return new int[]{hashMap.get(complement), i};
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
